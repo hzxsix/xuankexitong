eb8833ed75d029147f48ebdde2b79ca0aa08e626
xxgjc <76429165+xxgjc@users.noreply.github.com>
2021-01-06 03:24:01 -0600

    Add files via upload

--- cx6.cpp
+++ cx6.cpp
@@ -1,55 +1,64 @@
-#include <iostream>
-#include <iomanip>
+#include<iostream>
+#include <math.h>
 using namespace std;
-class vehicle//创建类
+
+class vehicle
 {
-public://公有
-	vehicle(int wh,int we);//构造函数
-protected://保护
-	int wheels;//数据
-	int weight;
-};
-vehicle::vehicle(int wh,int we)//构造函数定义
-{
-	wheels=wh;
-	weight=we;
-	cout<<"汽车轮子数量是：";
-	cin>>wheels;
-	cout<<endl;
-	cout<<"汽车重量是：";
-	cin>>weight;
-	cout<<endl;
-}
-class car: private vehicle//私有派生类car
-{
-	int passenger_load;//载人数
+protected:
+int wheels;
+int weight;
 public:
-	car(int wh,int we,int pa):vehicle(wh,we)
-	{
-		passenger_load=pa;
-		cout<<"passenger_load=";
-		cin<<passenger_load<<endl;
-	}
-};
-class truck:private vehicle
+vehicle(int wh, int we)
 {
-	int passenger_load;
-	int payload;
-public:
-	truck(int wh,int we,int pa,int pay):vehicle(wh,we)
-	{
-		passenger_load=pa;
-		payload=pay;
-		cout<<"passenger_load="；
-		cin>>passenger_load<<endl;
-		cout<<"payload";
-		cin>>payload>>endl;
-	}
+wheels = wh;
+weight = we;
 }
+void disp()
+{
+cout << "wheels=" << wheels << ",weight=" << weight << endl;
+}
+
+
+};
+class car:private vehicle
+{
+int passenger_load;
+public:
+car(int wh, int we, int pa) :vehicle(wh,we)
+{
+passenger_load = pa;
+}
+void disp()
+{
+vehicle::disp();
+cout << "passenger_load=" << passenger_load << endl;
+}
+
+
+};
+class truck :private vehicle
+{
+int passenger_load;
+int payload;
+public:
+truck(int wh, int we, int pa,int pay) :vehicle(wh, we)
+{
+passenger_load = pa;
+payload = pay;
+}
+void disp()
+{
+vehicle::disp();
+cout << "passenger_load=" << passenger_load << "payload=" << payload << endl;
+}
+};
 void main()
 {
-	vehicle v(100, 99);
-	car c(50, 40, 30);
-	truck t(20, 15, 10, 5);
-	system("pause");
+vehicle v(100, 99);
+v.disp();
+car c(50, 40, 30);
+c.disp();
+truck t(20, 15, 10, 5);
+t.disp();
+system("pause");
 }
